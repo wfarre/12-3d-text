6d3e7535ba4ffd14e83957e6a3f96a57d5d4b244
wfarre <william.farre@gmail.com>
2024-07-25 16:42:56 +0800

    commit

--- src/script.js
+++ src/script.js
@@ -1,8 +1,11 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import GUI from "lil-gui";
-import { FontLoader, TextGeometry } from "three/examples/jsm/Addons.js";
-import { log } from "three/examples/jsm/nodes/Nodes.js";
+import {
+  FontLoader,
+  RGBELoader,
+  TextGeometry,
+} from "three/examples/jsm/Addons.js";
 
 /**
  * Base
@@ -17,23 +20,40 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 //AXIS HELPER
-const axesHelper = new THREE.AxesHelper();
-scene.add(axesHelper);
+// const axesHelper = new THREE.AxesHelper();
+// scene.add(axesHelper);
 
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
-const matcapTexture = textureLoader.load("/textures/matcaps/8.png");
+const matcapTexture = textureLoader.load("/textures/matcaps/5.png");
+const metalTexture = textureLoader.load("/textures/matcaps/2.png");
+metalTexture.colorSpace = THREE.SRGBColorSpace;
 matcapTexture.colorSpace = THREE.SRGBColorSpace;
-const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
+// const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
+
+const material = new THREE.MeshPhysicalMaterial({ map: matcapTexture });
+const metalMaterial = new THREE.MeshPhysicalMaterial({ map: metalTexture });
+
+material.metalness = 0.6;
+material.roughness = 0.2;
+material.iridescence = 1;
+material.iridescenceIOR = 1;
+material.iridescenceThicknessRange = [100, 800];
+
+metalMaterial.metalness = 0.4;
+metalMaterial.roughness = 0.5;
+metalMaterial.iridescence = 1;
+metalMaterial.iridescenceIOR = 1;
+metalMaterial.iridescenceThicknessRange = [100, 800];
 
 /**
  * Fonts
  */
 const fontLoader = new FontLoader();
 fontLoader.load("/font/helvetiker_regular.typeface.json", (font) => {
-  const textGeometry = new TextGeometry("Hello Three.js", {
+  const textGeometry = new TextGeometry("Star Donuts", {
     font: font,
     size: 0.5,
     depth: 0.2,
@@ -47,8 +67,19 @@ fontLoader.load("/font/helvetiker_regular.typeface.json", (font) => {
 
   textGeometry.center();
 
-  const text = new THREE.Mesh(textGeometry, material);
+  const text = new THREE.Mesh(textGeometry, metalMaterial);
   scene.add(text);
+});
+
+/**
+ * Environment map
+ */
+const rgbeLoader = new RGBELoader();
+rgbeLoader.load("/stars.hdr", (environmentMap) => {
+  environmentMap.mapping = THREE.EquirectangularReflectionMapping;
+  scene.background = environmentMap;
+  scene.environment = environmentMap;
+  console.log(environmentMap);
 });
 
 /**
@@ -84,6 +115,18 @@ for (let i = 0; i < 100; i++) {
 // );
 
 // scene.add(cube);
+
+/**
+ * Lights
+ */
+const ambiantLight = new THREE.AmbientLight(0xffffff, 1);
+scene.add(ambiantLight);
+const pointLight = new THREE.PointLight(0xffffff, 30);
+pointLight.position.x = 0;
+pointLight.position.y = 3;
+pointLight.position.z = 4;
+// pointLight.lookAt(text.position);
+scene.add(pointLight);
 
 /**
  * Sizes
